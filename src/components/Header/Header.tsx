adef9a5d1e17aee4565e29c6324f5f1b386ef280
Michael <alpha.lucky.m@gmail.com>
2022-03-07 12:30:00 +0300

    init app

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import { memo } from "react"
+import { HeaderDescription, Navigate, WrapperHeader } from "./index.styled"
+
+const Header = () => {
+    return <WrapperHeader>
+        <HeaderDescription>
+
+        </HeaderDescription>
+        <Navigate>
+            
+        </Navigate>
+    </WrapperHeader>
+}
+
+
+export default memo(Header)
---
+++ src/components/Header/index.styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const WrapperHeader = styled.header`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+`
+export const HeaderDescription = styled.div`
+    width: 100%;
+    height: 100px;
+    background-color: red;
+`
+export const Navigate = styled.nav`
+    width: 100%;
+    height: 100px;
+    background-color: green;
+`
